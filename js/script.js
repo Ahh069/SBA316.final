bff319f1cbf92cb87697d914fc931a5c635176d5
Ahh069 <mshyt0577@gmail.com>
2024-03-21 18:42:59 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Cache at least one element using selectElementById.
+let firstName = document.getElementById('First Name');
+console.log(firstName);
+
+Cache at least one element using querySelector or querySelectorAll.
