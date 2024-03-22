eead6db185efc39346f59cb2ed84943a016c4ac9
Ahh069 <mshyt0577@gmail.com>
2024-03-21 20:59:23 -0400

    update

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ const container = document.querySelectorAll('div');
 console.log(container);
 
 // Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
+document.addEventListener("DOMContentLoaded", function() {
+    var firstNameInput = document.getElementById("firstName");
+    var lastNameLabel = firstNameInput.parentElement.nextElementSibling.firstChild;
+    
+    console.log("The label of the Last Name input field is:", lastNameLabel.textContent);
+});
+
+
+// Create at least one element using createElement.
+let aTag = document.createElement('a');
+aTag.textContent = "About Us";
+aTag.setAttribute('href', 'https://www.google.com');
+console.log(aTag);
+
+
