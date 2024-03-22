cdce13c0c15689e773943299df5dd606c1906e3f
Ahh069 <mshyt0577@gmail.com>
2024-03-21 20:41:05 -0400

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const title = document.getElementById('main-heading');
 console.log(title);
 
 // Cache at least one element using querySelector or querySelectorAll.
-const container = document.querySelector('div');
+const container = document.querySelectorAll('div');
 console.log(container);
 
 // Use the parent-child-sibling relationship to navigate between elements at least once (firstChild, lastChild, parentNode, nextElementSibling, etc.).
